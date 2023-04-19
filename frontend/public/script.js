fdb9964e952de50483291fcea15af06fd39f054d
BatoriTamas <batori.tamas@gmail.com>
2023-04-19 10:05:37 +0200

    file upload

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,45 +1,64 @@
 const rootElement = document.querySelector("#root");
-const imagesComponent = (title, url) =>  `
-    <div class="swiper-slide">
-      <div class="firstSlide">
-        <h3 class="title">${title}</h3>
-        <img src="public/img${url}"/>
-      </div>
+const imagesComponent = (title, url) => `
+  <div class="swiper-slide">
+    <div class="firstSlide">
+      <h3 class="title">${title}</h3>
+      <img src="public/img${url}"/>
     </div>
+  </div>
   `;
-
 const swiperWrapElement = document.querySelector(".swiper-wrapper");
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-fetch('/images')
-  .then(res => res.json())
-  .then(data => {
-  data.forEach(element => {
-    swiperWrapElement.insertAdjacentHTML(
-      "beforeend",
-      imagesComponent(element.title, element.url)
-    );
+fetch(`/images`)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((element) => {
+      swiperWrapElement.insertAdjacentHTML(
+        "beforeend",
+        imagesComponent(element.title, element.url)
+      );
+    });
   });
+
+const uploadBtn = document.querySelector(".upload-btn");
+uploadBtn.addEventListener("click", () => {
+  console.log("upload");
+
+  const formData = new FormData();
+  formData.append(
+    "image",
+    document.querySelector(`input[type="file"]`).files[0]
+    //ezt követően lehet Írni egy if statement, hogy megvizsgáljuk, hogy valóban van-e file kiválasztva
+  );
+  formData.append("title", document.querySelector(`input[name="title"]`).value);
+  formData.append(
+    "phName",
+    document.querySelector(`input[name="ph-name"]`).value
+  );
+  fetch("/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((resData) => {
+      console.log(resData);
+      swiperWrapElement.insertAdjacentHTML(
+        "beforeend",
+        imagesComponent(resData.title, resData.url)
+      );
+    });
 });
-    
